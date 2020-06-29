5972791243f848d4184e73616a3b33ea0794f58f
Marshmellowon <leo503801@gmail.com>
2020-06-30 02:47:08 +0900

    fin project

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, send_file
+from scrapper import get_jobs
+from exporter import save_to_file
 
 app = Flask("JobScrapper")
+
+# fake db
+db = {}
 
 
 @app.route('/')
@@ -11,7 +16,40 @@ def hello_world():
 @app.route('/report')
 def report():
     word = request.args.get('word')
-    return render_template("report.html", searchingBy=word)
+    if word:
+        word = word.lower()
+        existing_Jobs = db.get(word)
+        if existing_Jobs:
+            jobs = existing_Jobs
+        else:
+            jobs = get_jobs(word)
+            db[word] = jobs
+    else:
+        return redirect("/")
+    return render_template(
+        "report.html",
+        searchingBy=word,
+        resultsNumber=len(jobs),
+        jobs=jobs
+    )
+
+
+@app.route("/export")
+def export():
+    try:
+        word = request.args.get('word')
+        if not word:
+            raise Exception();
+        word = word.lower()
+        jobs = db.get(word)
+        if not word:
+            raise Exception()
+        save_to_file(jobs)
+        return send_file("jobs.csv")
+        return f"Generate CSV for {word}"
+
+    except:
+        return redirect("/")
 
 
 if __name__ == '__main__':
---
+++ exporter.py
@@ -0,0 +1,11 @@
+import csv
+
+
+def save_to_file(jobs):
+    print("save_to 에서 jobs의 타입은: ", type(jobs))
+    file = open("jobs.csv", mode="w", encoding='utf-8')
+    writer = csv.writer(file)
+    writer.writerow(['title', 'company', 'location', 'link'])
+    for job in jobs:
+        writer.writerow(job.values())
+    return
